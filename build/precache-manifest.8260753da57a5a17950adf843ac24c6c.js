f4404d1f3d8ec52efdb69388bbe1685c3b3a9689
Anita <97972576+0907anita@users.noreply.github.com>
2022-04-01 17:44:18 +0530

    Add files via upload

---
+++ build/precache-manifest.8260753da57a5a17950adf843ac24c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b300e9d5eb05949270b9db1515763a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e02f2fcdc4df2105e93a",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "ae567edcc927c0b7b1f5",
+    "url": "/static/js/2.293b7288.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.293b7288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02f2fcdc4df2105e93a",
+    "url": "/static/js/main.1ba4fd5b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
